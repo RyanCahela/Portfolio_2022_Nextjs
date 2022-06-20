0c5208732338ae88d29412f74004238907a15bf9
RyanCahela <6194542+RyanCahela@users.noreply.github.com>
2022-06-20 14:21:48 -0700

    inital commit, tailwind installed

---
+++ pages/index.js
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div className="">
+      <h1 className="bg-black text-green-500 font-bold text-3xl">
+        Hello world
+      </h1>
+    </div>
+  );
+}
