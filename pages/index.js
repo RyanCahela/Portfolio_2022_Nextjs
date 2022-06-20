0d05988db131712250c0b4a2854391e6d9f7cc3b
RyanCahela <6194542+RyanCahela@users.noreply.github.com>
2022-06-20 14:31:00 -0700

    created section components

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,19 @@
+import Header from "../components/Sections/Header/Header";
+import Hero from "../components/Sections/Hero/Hero";
+import AboutMe from "../components/Sections/About_Me/AboutMe";
+import Portfolio from "../components/Sections/Portfolio/Protfolio";
+import Contact from "../components/Sections/Contact/Contact";
+import Footer from "../components/Sections/Footer/Footer";
+
 export default function Home() {
   return (
     <div className="">
-      <h1 className="bg-black text-green-500 font-bold text-3xl">
-        Hello world
-      </h1>
+      <Header />
+      <Hero />
+      <AboutMe />
+      <Portfolio />
+      <Contact />
+      <Footer />
     </div>
   );
 }
