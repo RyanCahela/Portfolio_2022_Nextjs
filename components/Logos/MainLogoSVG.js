429036590cd41189aa7bb08eabc177b1649b7f56
RyanCahela <6194542+RyanCahela@users.noreply.github.com>
2022-06-22 12:00:17 -0700

    built mobile footer component

--- components/Logos/MainLogoSVG.js
+++ components/Logos/MainLogoSVG.js
@@ -4,7 +4,7 @@ const MainLogoSVG = () => {
   return (
     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 61 32">
       <path
-        fill="#33323D"
+        fill="currentColor"
         fillRule="evenodd"
         d="M60.082 5.878L44.408 32 28.735 5.878h31.347zM15.673 0l15.674 26.122H0L15.673 0z"
       />
--- components/Sections/Footer/Footer.js
+++ components/Sections/Footer/Footer.js
@@ -1,7 +1,54 @@
 import React from "react";
+import Link from "next/link";
+import MainLogoSVG from "../../Logos/MainLogoSVG";
+import GithubLogoSVG from "../../Logos/GithubLogoSVG";
+import TwitterLogoSVG from "../../Logos/TwitterLogoSVG";
+import LinkedinLogoSVG from "../../Logos/LinkedinLogoSVG";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <footer className="flex flex-col items-center py-14 gap-10 text-center text-white bg-grayish-dark-blue w-screen relative left-1/2 -translate-x-1/2">
+      <div className="w-15 h-8">
+        <MainLogoSVG />
+      </div>
+      <nav>
+        <ul className="flex flex-col gap-8 uppercase tracking-[2px] text-xs">
+          <li>
+            <Link href="/">
+              <a>Home</a>
+            </Link>
+          </li>
+          <li>
+            <Link href="/">
+              <a>Portfolio</a>
+            </Link>
+          </li>
+          <li>
+            <Link href="/contact">
+              <a>Contact Me</a>
+            </Link>
+          </li>
+        </ul>
+      </nav>
+      <ul className="flex gap-4">
+        <li className="w-6 h-6">
+          <a href="https://github.com/RyanCahela">
+            <GithubLogoSVG />
+          </a>
+        </li>
+        <li className="w-6 h-6">
+          <a href="https://twitter.com/">
+            <TwitterLogoSVG />
+          </a>
+        </li>
+        <li className="w-6 h-6">
+          <a href="https://www.linkedin.com/">
+            <LinkedinLogoSVG />
+          </a>
+        </li>
+      </ul>
+    </footer>
+  );
 };
 
 export default Footer;
