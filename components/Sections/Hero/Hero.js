db9daee01babf919b0ff12eccdf8e1f22b960c5f
RyanCahela <6194542+RyanCahela@users.noreply.github.com>
2022-06-23 12:55:58 -0700

    created hero section for tablet view

--- components/Sections/Hero/Hero.js
+++ components/Sections/Hero/Hero.js
@@ -7,18 +7,20 @@ import PrimaryButton from "../../Buttons/PrimaryButton";
 
 const Hero = () => {
   return (
-    <section className="pb-24">
+    <section className="pb-24 md:relative">
       <Image src={heroImage} alt="computer on a desk" layout="responsive" />
-      <div className="pt-6 pb-8">
-        <Title1>
-          Hi, I&apos;m Ryan Cahela and I love building beautiful websites
-        </Title1>
+      <div className="md:absolute md:bottom-24 md:left-0 md:w-[32.125rem] md:h-[17.375rem] md:bg-white md:pt-14">
+        <div className="pt-6 pb-8">
+          <Title1>
+            Hi, I&apos;m Ryan Cahela and I love building beautiful websites
+          </Title1>
+        </div>
+        <Link href="/#Portfolio">
+          <a>
+            <PrimaryButton textContent="Portfolio" />
+          </a>
+        </Link>
       </div>
-      <Link href="/#Portfolio">
-        <a>
-          <PrimaryButton textContent="Portfolio" />
-        </a>
-      </Link>
     </section>
   );
 };
