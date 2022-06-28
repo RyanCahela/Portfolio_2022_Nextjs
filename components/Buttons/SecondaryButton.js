6452feb3c9714076d6f7d12a7a8ba083ce7504c8
RyanCahela <6194542+RyanCahela@users.noreply.github.com>
2022-06-28 11:54:13 -0700

    created button with cut corner effect

--- components/Buttons/SecondaryButton.js
+++ components/Buttons/SecondaryButton.js
@@ -11,23 +11,34 @@ const buttonClasses = `
   uppercase
 
   ${/* Colors */ ""}
-  bg-white
   text-greyish-dark-blue
   
   
   ${/* Misc */ ""}
   cursor-pointer
+  ${
+    /*
+
   outline
   outline-2
   outline-grayish-dark-blue
 
   hover:bg-grayish-dark-blue
+  
+  */ ""
+  }
+
   hover:text-very-light-gray
+  focus:text-very-light-gray
+  transition-colors
+
 
   disabled:text-grayish-dark-blue/20
   disabled:outline-grayish-dark-blue/20
   disabled:cursor-not-allowed
   disabled:hover:bg-white
+
+  cut-corners
 `;
 
 const SecondaryButton = ({ textContent, disabled = null }) => {
